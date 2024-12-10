6313ca06cba39f33792681b027707d3cae3c8d3b
ClaRa-git <rameau.claire.cr@gmail.com>
2024-12-10 09:28:29 +0100

    Mise en place de la page d'accueil qui liste tous les biens (rental\user\list). Mise en place de App et de RentalController

--- app/src/Model/Entity/Rental.php
+++ app/src/Model/Entity/Rental.php
@@ -48,7 +48,7 @@ class Rental extends Entity
         return $this;
     }
 
-    // Foreign key de la table type_logement
+    // Foreign key / Liaison avec la table type_logement
     protected int $type_logement;
     public function getTypeLogementId(): int
     {
@@ -63,7 +63,7 @@ class Rental extends Entity
         $this->type_logement = $id;
     }
 
-    // Foreign key de la table addresses
+    // Foreign key / Liaison avec la table addresses
     protected int $address_id;
     public function getAddressId(): int
     {
