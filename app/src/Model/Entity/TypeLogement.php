ea657cb2d93a41161cef31c4e3d359fc94acd77f
ClaRa-git <rameau.claire.cr@gmail.com>
2024-12-10 09:19:00 +0100

    Création de l'Entity TypeLogement et du Repository TypeLogementRepository

--- app/src/Model/Entity/TypeLogement.php
+++ app/src/Model/Entity/TypeLogement.php
@@ -7,7 +7,7 @@ class TypeLogement extends Entity
 {
     protected string $label_type_logement;
     public function getName(): string { return $this->label_type_logement; }
-    public function setName(int $value): self
+    public function setName(int $value): self 
     {
         $this->label_type_logement = $value;
         return $this;
--- app/src/Model/Repository/TypeLogementRepository.php
+++ app/src/Model/Repository/TypeLogementRepository.php
@@ -17,11 +17,21 @@ class TypeLogementRepository extends Repository
         return 'type_logement';
     }
 
+    /**
+     * Récupère un type de logement par son id
+     * @param int $id
+     * @return TypeLogement|null
+     */
     public function getById(int $id): ?TypeLogement
     {
         return $this->readById(TypeLogement::class, $id);
     }
 
+    /**
+     * Récupère tous les types de logement
+     * pas de paramètre
+     * @return array
+     */
     public function getAll(): array
     {
         return $this->readAll(TypeLogement::class);
